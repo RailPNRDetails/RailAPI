086232ea8919d3588df943e3e46a5c9c5bd282ab
ashwinreddych <ashwinreddych@gmail.com>
2017-04-06 23:19:35 -0400

    Adding Cancelled Trains Logic (Initial commit)

--- src/main/java/com/railpnrdetails/api/RailAPIController.java
+++ src/main/java/com/railpnrdetails/api/RailAPIController.java
@@ -1,37 +1,64 @@
 package com.railpnrdetails.api;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.http.HttpStatus;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
+
+import org.springframework.http.HttpStatus;
 
 @RestController
 public class RailAPIController {
+	
+	public static final String JSON_MEDIA_TYPE = "application/json";
 
-
-
-
-//	@RequestMapping(method = RequestMethod.GET, value = "/{userId}")
-//	public String test(@PathVariable String userId, @PathVariable Long bookmarkId) {
-//		return userId;
-//	}
-
-	@RequestMapping(value = "/api", method= RequestMethod.GET)
+	@RequestMapping(value = "/api", method = RequestMethod.GET)
 	@ResponseBody
 	public ResponseEntity<Object> get() {
-		
+
 		Map<String, Object> response = new HashMap<String, Object>();
 		response.put("code", "200");
-		return new ResponseEntity<Object>(response,HttpStatus.OK);
+		return new ResponseEntity<Object>(response, HttpStatus.OK);
+	}
+
+	
+	/*
+	 * @param headers
+	 * @param request
+	 * @return String
+	 * @throws
+	 */
+	@RequestMapping(value = "/api/cancelledTrains/{ID}", method= RequestMethod.GET, consumes = JSON_MEDIA_TYPE, produces = JSON_MEDIA_TYPE)
+	@ResponseBody
+	public void getcancelledTrainsList(@PathVariable("ID") String ID, HttpServletRequest request, HttpServletResponse response) throws JsonSyntaxException, IOException {
+
+		Gson gson = new Gson();
+		
+		JsonObject responseData = new JsonObject();
+		responseData.addProperty("ID", ID);
+		
+		gson.fromJson(responseData, JsonObject.class).getAsJsonObject().toString().getBytes();
+		response.setContentType(JSON_MEDIA_TYPE);
+		
+		try{
+			response.getOutputStream().write(gson.fromJson(responseData, JsonObject.class).toString().getBytes());
+		}finally {
+			response.getOutputStream().close();
+		}
+		
 	}
 
 }
