97b576461cafe4bc605146665045ebc9c5c67c23
ashwinreddych <ashwinreddych@gmail.com>
2017-04-11 23:10:03 -0400

    Added LiveTrainStatus code and logic to connect RailwayApi

--- src/main/java/com/railpnrdetails/api/RailAPIController.java
+++ src/main/java/com/railpnrdetails/api/RailAPIController.java
@@ -1,6 +1,12 @@
 package com.railpnrdetails.api;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -60,5 +66,87 @@ public class RailAPIController {
 		}
 		
 	}
+	
+	/*
+	 * @param headers
+	 * @param request
+	 * @return String
+	 * @throws
+	 */
+	@RequestMapping(value = "/api/liveTrainStatus", method= RequestMethod.POST, consumes = JSON_MEDIA_TYPE, produces = JSON_MEDIA_TYPE)
+	@ResponseBody
+	public void getLiveTrainsStatusList(HttpServletRequest request, HttpServletResponse response) throws JsonSyntaxException, IOException {
+
+		Gson gson = new Gson();
+		
+		
+		JsonObject responseData = testService( request,  response);
+		
+		
+		
+		//gson.fromJson(responseData, JsonObject.class).getAsJsonObject().toString().getBytes();
+		response.setContentType(JSON_MEDIA_TYPE);
+		
+		try{
+			response.getOutputStream().write(gson.fromJson(responseData, JsonObject.class).toString().getBytes());
+		}finally {
+			response.getOutputStream().close();
+		}
+		
+	}
+	
+	private JsonObject testService(HttpServletRequest request, HttpServletResponse response){
+		
+		JsonObject responseData = new JsonObject();
+	try {
+
+			int trainNumber = 12345;
+			String date = "20170411"; //YYYYMMDD
+			//Test service
+			URL url = new URL("http://api.railwayapi.com/live/train/"+trainNumber+"/doj/"+date+"/apikey/0vfjouj0");
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			
+			
+			Gson gson = new Gson();
+		
+			conn.setDoOutput(true);
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Content-Type", "application/json");
+
+
+			OutputStream os = conn.getOutputStream();
+			os.flush();
+
+			if (conn.getResponseCode() != 200) {
+				throw new RuntimeException("Failed : HTTP error code : "
+						+ conn.getResponseCode());
+			}
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					(conn.getInputStream())));
+
+			String output;
+			System.out.println("Output from Server .... \n");
+			while ((output = br.readLine()) != null) {
+
+				responseData.addProperty("ResponseFromAPI", output);
+				System.out.println(responseData);
+			}
+
+			conn.disconnect();
+
+		} catch (MalformedURLException e) {
+
+			e.printStackTrace();
+		} catch (IOException e) {
+ 
+			e.printStackTrace();
+
+		}
+	
+	return responseData;
+	}
+	
+	 
 
 }
