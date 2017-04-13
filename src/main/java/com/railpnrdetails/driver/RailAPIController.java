b55b7ef0b12683c9f31e6309929a4e9028ac05ba
ashwinreddych <ashwinreddych@gmail.com>
2017-04-12 23:07:17 -0400

    packageing chnages

--- src/main/java/com/railpnrdetails/driver/RailAPIController.java
+++ src/main/java/com/railpnrdetails/driver/RailAPIController.java
@@ -1,4 +1,4 @@
-package com.railpnrdetails.api;
+package com.railpnrdetails.driver;
 
 import java.io.BufferedReader;
 import java.io.IOException;
--- src/main/java/com/railpnrdetails/driver/RailApiApplication.java
+++ src/main/java/com/railpnrdetails/driver/RailApiApplication.java
@@ -1,4 +1,4 @@
-package com.railpnrdetails.api;
+package com.railpnrdetails.driver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
