0ac1f2efa5ae657564b205d94558b38060bda1b5
ashwinreddych <ashwinreddych@gmail.com>
2017-04-12 23:10:31 -0400

    Build issue fixes

--- src/main/java/com/railpnrdetails/driver/RailApiApplication.java
+++ src/main/java/com/railpnrdetails/driver/RailApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.railpnrdetails.api")
+@ComponentScan("com.railpnrdetails.driver")
 public class RailApiApplication {
 
 	public static void main(String[] args) {
